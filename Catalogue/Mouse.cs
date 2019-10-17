2daaf8598f1ba536a86ddb90e2536d1275cc691e
adharsh8 <adharsh.s@psiog.com>
2019-10-17 18:42:58 +0530

    Update Mouse.cs

--- Catalogue/Mouse.cs
+++ Catalogue/Mouse.cs
@@ -103,4 +103,106 @@ namespace Catalogue
             } while (user_choice == "y");
         }
     }
+
+    class Mouseadd
+        {
+        public void updation()
+        {
+            Console.WriteLine("Enter the operation to be done..(1/2/3)");
+            Console.WriteLine();
+            Console.WriteLine("-----> 1.) ADD  2.) DELETE  3.) EDIT <------");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+    
+            switch(choice)
+            {
+                case 1:
+                    add();
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    delete();
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    edit();
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice - Please enter 1/2/3");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+        public void add()
+            {
+            Console.WriteLine("Enter the details of Mouse to be added");
+            Console.ReadKey();
+            Console.Write("Enter the ID :");
+            String x = Console.ReadLine();
+
+            Console.Write("Enter the Brand :");
+            String y = Console.ReadLine();
+
+            Console.Write("Enter the Model :");
+            String z = Console.ReadLine();
+
+            Console.Write("Enter the Price :");
+            String w = Console.ReadLine();
+          
+            XDocument xDocument = XDocument.Load("Mouse.xml");
+            XElement root= xDocument.Element("Mouses");
+            IEnumerable<XElement> rows = root.Descendants("Mouse");
+            XElement firstRow= rows.First();
+            firstRow.AddBeforeSelf(
+            new XElement("Mouse",
+            new XElement("ID", x),
+            new XElement("brand", y),
+            new XElement("model",z),
+            new XElement("price",w)));
+            xDocument.Save("Mouse.xml");
+           
+            Console.WriteLine("Mouse Added and Saved");
+       
+            } 
+        public void delete()
+        {
+            Console.Write("Enter the MOUSE_ID to be deleted :");
+            String user_id = Console.ReadLine();
+            
+            XElement xelement = XElement.Load("Mouse.xml");
+            IEnumerable<XElement> Mouses = xelement.Elements();
+            var x = from Mouse in xelement.Elements("Mouse")
+                    where (string)Mouse.Element("ID") == user_id
+                    select Mouse;
+            x.Remove();
+            xelement.Save("Mouse.xml");
+            Console.WriteLine("The MOUSE_ID "+ user_id +"is deleted Successfully");
+            
+        }
+        public void edit()
+        {
+            Console.Write("Enter the Id to be edited :");
+            String user_id = Console.ReadLine();
+          
+            Console.Write("Enter the new updated price of the  "+user_id +"  :");
+            String pricenew = Console.ReadLine();
+
+             XElement xelement = XElement.Load("Mouse.xml");
+            IEnumerable<XElement> Mouses = xelement.Elements();
+            var x = from Mouse in xelement.Elements("Mouse")
+                    where (string)Mouse.Element("ID") == user_id
+                    select Mouse;
+  
+        foreach (XElement id in x)  
+        {            
+            String price_detail = id.Element("price").Value;
+          
+            id.SetElementValue("price", pricenew);  
+        }  
+            xelement.Save("Mouse.xml");
+            Console.WriteLine("Editing done");
+        }
+        }
 }
+     
